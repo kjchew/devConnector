fee67e3b8d677652046393a987b86ef7e74c2812
Chew, Kar Joon <kar.joon.chew@merck.com>
2018-10-01 10:24:57 +0800

    Added Configuration and preparing for routing

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI || "mongodb://chewkj:Pass1234@ds145415.mlab.com:45415/heroku_mpg78m2n",
+    secretOrKey: process.env.SECRET_OR_KEY
+  };
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const dbURL = require('./config/keys').mongoURI;
+
+mongoose.connect(dbURL).then(
+    () => console.log(`DB Connection Established on ${dbURL}`)
+).catch(err => console.log(err));
+
+const app = express();
+
+app.get('/', (req, res) =>
+    res.send('Hello World'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('api/posts', posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
