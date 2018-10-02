25a9ab57d5c3ac8d4c110a802b6cf58cbb81e28d
Kar Joon Chew <chewkj@gmail.com>
2018-10-02 23:32:49 +0800

    Basic login

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,4 +45,33 @@ router.post('/register', (req, res) => {
         })
        
 })
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    // Find user by email
+    User.findOne({ email }).then(user => {
+      // Check for user
+      if (!user) {
+        const errors = {email: 'User not found'};
+        return res.status(404).json(errors);
+      }
+  
+      // Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User Matched
+          res.json({msg: "Login Success"});
+        } else {
+          const errors = {password : 'Password incorrect'};
+          return res.status(400).json(errors);
+        }
+      });
+    });
+  });
 module.exports = router;
